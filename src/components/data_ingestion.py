a64232d20603b87f5fc65f003055971dbecdc464
sohamnp06 <sohampashilkar84@gmail.com>
2025-10-21 21:42:06 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ from src.exception import customException
 from sklearn.model_selection import train_test_split
 from src.components.data_transformation import dataTransformationConfig
 from src.components.data_transformation import dataTransformation
+from src.components.model_trainer import modelTrainerConfig
+from src.components.model_trainer import modelTrainer
+
+
 #USE DATA CLASS WHEN YOU ONLY WAN TO INITIALIZE VARIABLE IF IT ALSO HAS FUNCTION GO WITH NORMAL CLASSES
 @dataclass
 class dataIngestionConfig:
@@ -54,6 +58,7 @@ if __name__ == "__main__":
     train_data,test_data=obj.initiate_data_ingestion()
             
     data_transformation=dataTransformation()
-    data_transformation.initiate_data_tranformation(train_data,test_data)    
+    train_arr,test_arr,_=data_transformation.initiate_data_tranformation(train_data,test_data)    
     
-    
+    modelTrainer=modelTrainer()
+    print(modelTrainer.initiate_model_trainer(train_arr,test_arr))
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,12 @@
 import os
 import sys
-import dill
-import numpy as np
+
+import numpy as np 
 import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import customException
 
@@ -17,3 +21,36 @@ def save_object(file_path,obj):
             
     except Exception as e:
         raise customException(e,sys)
+    
+    
+def evaluate_models(X_train, Y_train,X_test,Y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            # para=param[list(models.keys())[i]]
+
+            # gs = GridSearchCV(model,para,cv=3)
+            # gs.fit(X_train,Y_train)
+
+            # model.set_params(**gs.best_params_)
+            model.fit(X_train,Y_train)
+
+            #model.fit(X_train, Y_train)  # Train model
+
+            Y_train_pred = model.predict(X_train)
+
+            Y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(Y_train, Y_train_pred)
+
+            test_model_score = r2_score(Y_test, Y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise customException(e, sys)
+  
